82248eda04febd5dedcffef54a98c8403a3ff327
CryptoACTION <cryptoaction@MacBook-Pro-von-Oliver.local>
2026-05-12 23:39:47 +0200

    Add dropdown menu for map

--- src/components/legal/Legal.tsx
+++ src/components/legal/Legal.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import img from "../../assets/layers_overview.png"
-import GlobalLayer from "./GlobalLayer.tsx";
-import NationalLayer from "./NationalLayer.tsx";
-import EULayer from "./EULayer.tsx";
+import Map from "./Map.tsx";
 
 const Legal: React.FC = () => {
     return (
         <section className="content-section">
             <h2>LEGAL</h2>
             <img src={img} alt="overview" className="layer-image"/>
-            <NationalLayer/>
-            <EULayer/>
-            <GlobalLayer/>
+            <Map/>
         </section>
     );
 };
