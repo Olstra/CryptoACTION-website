05a9611b5ab4ac1fc9433a8bdd467eab9a28b1de
olstra <github.audition223@passmail.com>
2026-08-04 22:09:06 +0200

    Make 'about' component pretty. Add simple site reactivity.

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,7 +3,8 @@ import styles from "./About.module.sass";
 import {
   projectTitle,
   projectSubtitle,
-  projectDescription,
+  projectBackground,
+  projectObjectives,
 } from "./projectDescriptionText.tsx";
 
 export const About: React.FC = () => {
@@ -11,8 +12,30 @@ export const About: React.FC = () => {
     <section className="content-section">
       <h1 className={styles.aboutTitle}>{projectTitle}</h1>
       <p className={styles.aboutSubtitle}>{projectSubtitle}</p>
-      <br />
-      <p className={styles.aboutParagraph}>{projectDescription}</p>
+
+      <div className={styles.sectionBlock}>
+        <h2 className={styles.sectionHeading}>Background</h2>
+        {projectBackground.map((paragraph, index) => (
+          <p key={index} className={styles.aboutParagraph}>
+            {paragraph}
+          </p>
+        ))}
+      </div>
+
+      <div className={styles.sectionBlock}>
+        <h2 className={styles.sectionHeading}>Objectives</h2>
+        <div className={styles.objectivesGrid}>
+          {projectObjectives.map((obj) => (
+            <div key={obj.code} className={styles.objectiveCard}>
+              <span className={styles.objectiveCode}>{obj.code}</span>
+              <div>
+                <h3 className={styles.objectiveTitle}>{obj.title}</h3>
+                <p className={styles.objectiveDesc}>{obj.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
